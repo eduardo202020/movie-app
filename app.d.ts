01007b9f00f594423666714fb2b63908131c8a02
Eduardo26 <jguevaral@uni.pe>
2023-06-22 17:35:10 -0500

    Set up project

---
+++ app.d.ts
@@ -0,0 +1,22 @@
+/// <reference types="nativewind/types" />
+
+interface moviesDataProps {
+  id: number;
+  title: string;
+  backgroundPoster: string;
+  image: string;
+  description: string;
+  releaseDate: string;
+  releaseYear: string;
+  runTime: string;
+}
+
+interface personDataProps {
+  name: string;
+  birthPlace: string;
+  birthday: string;
+  knownFor: string;
+  gender: string;
+  popularity: string;
+  biography: string;
+}
